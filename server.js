b0a3f511ce121518bb1c2a0c4943c2c358e02451
max <maxonchil@gmail.com>
2020-05-04 15:32:14 +0300

    test12

--- server.js
+++ server.js
@@ -1,32 +1,41 @@
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const config = require("config");
+// const todosRouter = require("./api/routing/todos.router");
+// const log4js = require("log4js");
+// const logger = log4js.getLogger();
+// const dbConnection = require("./api/utilits/dbConnection");
+// const { ERROR_LOG } = require("./data/logs.json");
+// const { level } = config.get("logger");
+// const path = require("path");
+
+// const { port: serverPort } = config.get("webServer");
+
+// logger.level = level;
+
+// // const dbConnect = dbConnection();
+// // if (!dbConnect) {
+// //   return logger.error(ERROR_LOG.DB_CONNECTION);
+// // }
+
+// app.use(express.static(__dirname, "/src"));
+// app.use(express.json());
+// app.use(cors());
+
+// app.use("/api/todos", todosRouter);
+
+// app.get("/", (req, res) => res.sendFile(__dirname, +"src/index.html"));
+
+// app.listen(serverPort, () =>
+//   console.log(`Server started at port ${serverPort}`)
+// );
+
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const config = require("config");
-const todosRouter = require("./api/routing/todos.router");
-const log4js = require("log4js");
-const logger = log4js.getLogger();
-const dbConnection = require("./api/utilits/dbConnection");
-const { ERROR_LOG } = require("./data/logs.json");
-const { level } = config.get("logger");
 const path = require("path");
-
-const { port: serverPort } = config.get("webServer");
-
-logger.level = level;
-
-// const dbConnect = dbConnection();
-// if (!dbConnect) {
-//   return logger.error(ERROR_LOG.DB_CONNECTION);
-// }
-
-app.use(express.static(__dirname, "/src"));
-app.use(express.json());
-app.use(cors());
-
-app.use("/api/todos", todosRouter);
-
-app.get("/", (req, res) => res.sendFile(__dirname, +"src/index.html"));
-
-app.listen(serverPort, () =>
-  console.log(`Server started at port ${serverPort}`)
-);
+app.use(express.static(__dirname + "/dist/pet-project-todos"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/pet-project-todos/index.html"));
+});
+app.listen(8080);
