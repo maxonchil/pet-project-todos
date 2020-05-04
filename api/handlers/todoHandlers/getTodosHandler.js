aabb965472b0cd2cf64ea983e89ee0e367fbd03c
max <maxonchil@gmail.com>
2020-05-04 19:04:47 +0300

    ..

--- api/handlers/todoHandlers/getTodosHandler.js
+++ api/handlers/todoHandlers/getTodosHandler.js
@@ -1,12 +1,16 @@
-const { Todo } = require('../../../schemas/todo.schema');
-const errorHandler = require('../error.handler');
-const success = require('../../utilits/successResponse');
-const { SUCCESS_LOG } = require('../../../data/logs.json');
+const { Todo } = require("../../../schemas/todo.schema");
+const errorHandler = require("../error.handler");
+const success = require("../../utilits/successResponse");
+const { SUCCESS_LOG } = require("../../../data/logs.json");
 
 const getTodosHandler = (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader("Content-Type", "application/json");
+
   Todo.find({})
-    .then((todos) => res.json(success(SUCCESS_LOG.SENDED, { todos })))
+    .then((todos) => {
+      console.log("im here");
+      res.json(success(SUCCESS_LOG.SENDED, { todos }));
+    })
     .catch((error) => errorHandler(error.message, res));
 };
 module.exports = getTodosHandler;
