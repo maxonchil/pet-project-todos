0a7c1cb783b4c9d498005076fd18522776eccd91
max <maxonchil@gmail.com>
2020-05-04 15:48:35 +0300

    test12

--- server.js
+++ server.js
@@ -34,8 +34,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-app.use(express.static(__dirname + "/dist/pet-project-todos"));
+app.use(express.static(__dirname + "/dist/todos"));
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/pet-project-todos/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/todos/index.html"));
 });
 app.listen(8080);
