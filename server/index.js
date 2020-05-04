d39798880685feb6e6d36c747daa752d53462668
max <maxonchil@gmail.com>
2020-05-04 13:06:30 +0300

    test3

--- server/index.js
+++ server/index.js
@@ -10,10 +10,6 @@ const { ERROR_LOG } = require('./data/logs.json');
 const { level } = config.get('logger');
 const path = require('path');
 
-app.use(express.static(__dirname + '/FE'));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/FE/src/index.html'));
-});
 const { port: serverPort } = config.get('webServer');
 
 logger.level = level;
@@ -23,10 +19,13 @@ if (!dbConnect) {
   return logger.error(ERROR_LOG.DB_CONNECTION);
 }
 
+app.use(express.static(__dirname, 'FE/src'));
 app.use(express.json());
 app.use(cors());
 
 app.use('/api/todos', todosRouter);
+
+app.get('/', (req, res) => res.sendFile(__dirname, +'FE/src/index.html'));
 
 app.listen(serverPort, () =>
   console.log(`Server started at port ${serverPort}`)
